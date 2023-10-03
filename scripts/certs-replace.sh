767a98ca614fd46d2a4c6434d09b7efef82f9395
vlobzakov <39057925+vlobzakov@users.noreply.github.com>
2023-10-03 13:02:16 +0300

    Update certs-replace.sh

--- scripts/certs-replace.sh
+++ scripts/certs-replace.sh
@@ -1,4 +1,4 @@
 lead='^.*\/\*\* Option 1'
 tail='^.*\/\*\* Option 2'
-sed -e "/$lead/,/$tail/{ /$lead/{p; r $baseUrl/scripts/valid_certs.txt
+sed -e "/$lead/,/$tail/{ /$lead/{p; r valid_certs.js
 }; /$tail/p; d }" /home/jelastic/.node-red/settings.js
