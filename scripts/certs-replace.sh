84275644854f868db309dc01d3d84413772b6365
vlobzakov <39057925+vlobzakov@users.noreply.github.com>
2023-10-03 14:53:53 +0300

    Update certs-replace.sh

--- scripts/certs-replace.sh
+++ scripts/certs-replace.sh
@@ -2,3 +2,4 @@ lead='^.*\/\*\* Option 1'
 tail='^.*\/\*\* Option 2'
 sed -e "/$lead/,/$tail/{ /$lead/{p; r valid_certs.js
 }; /$tail/p; d }" -i /home/jelastic/.node-red/settings.js
+sed 's/\/\/requireHttps/requireHttps/g' -i /home/jelastic/.node-red/settings.js
